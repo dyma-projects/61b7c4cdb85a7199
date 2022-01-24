618f25da330e6d08004034a7a98a756b3c7b3a49
Marko_M7 <mandalinic.marc@gmail.com>
2022-01-24 11:24:06 +0100

    Angular - Projet4

---
+++ src/app/exercice-1/color.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appColor]'
+})
+export class ColorDirective {
+
+  @HostBinding('style.color') couleur?: string;
+  @HostListener('window:keydown', ['$event']) windowClick($event) {
+    if ($event.key == "ArrowUp") {
+      this.couleur = 'red'
+    } else if ($event.key == "ArrowDown") {
+      this.couleur = 'green'
+    } else if ($event.key == "ArrowLeft") {
+      this.couleur = 'orange'
+    } else if ($event.key == "ArrowRight") {
+      this.couleur = 'blue'
+    } else {
+      this.couleur = 'black'
+    }
+  }
+
+  constructor() { }
+
+}
--- src/app/exercice-1/exercice-1.component.ts
+++ src/app/exercice-1/exercice-1.component.ts
@@ -6,14 +6,6 @@ import { Component, OnInit, HostListener } from '@angular/core';
 })
 export class Exercice1Component implements OnInit {
 
-  // Ce Hostlistener permet d'écouter un événement sur l'objet window global;
-  // Peut-être que cela vous sera utile pour votre directive ;)
-  // (Il vous faudra bien sûr le déplacer dans celle-ci)
-
-  // @HostListener('window:click', ['$event']) windowClick($event) {
-  //   console.log($event);
-  // }
-
   constructor() {
   }
 
